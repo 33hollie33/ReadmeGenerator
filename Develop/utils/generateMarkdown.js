bec1a1174e6cfc6b9156e8f0784d488b6a6d97b4
33hollie33 <hollybarnhurst@gmail.com>
2023-06-26 16:13:45 +0100

    start

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,37 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'APACHE2.0':
+      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+    case 'GPL3.0':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'BSD3':
+      return '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
+    case 'None':
+      return '';
+    default:
+      return '';
+  }
+
+}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+
+
+module.exports = generateMarkdown;
